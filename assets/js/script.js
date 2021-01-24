32a246e238b61f40fad88d6e672b186322f059f8
Paul Istratoaie <p4ul.gg@gmail.com>
2021-01-24 16:36:15 +0000

    feat(nav): implement dynamic content and url navigation
    Implement dynamic navigation in order to keep the player running while navigating through website
    
    BREAKING CHANGE: Chanege the way user navigate the website

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,102 +1,38 @@
-window.onkeydown = function (e) {
-    return !(e.keyCode == 32);
+// Navigation links event listeners for dynamic page load
+document.querySelectorAll('.nav-link').forEach(function (button) {
+    button.addEventListener('click', function (e) {
+        e.preventDefault();
+        var url = button.getAttribute('href');
+
+        // https://gomakethings.com/getting-html-with-fetch-in-vanilla-js/
+        // Fetch the content of the destination page
+        fetch(url).then(function (response) {
+            return response.text();
+        }).then(function (html) {
+            // Parse the response text to html object
+            var parser = new DOMParser();
+            var doc = parser.parseFromString(html, 'text/html');
+            processAjaxData(doc, url);
+        }).catch(function (err) {
+            // There was an error
+            console.warn('Something went wrong.', err);
+        });
+    });
+});
+
+// https://stackoverflow.com/a/3354511
+function processAjaxData(doc, urlPath) {
+    var html = doc.querySelector('html').innerHTML;
+    var title = doc.querySelector('title').innerHTML;
+    var body = doc.querySelector('body').innerHTML;
+    document.getElementById("content").innerHTML = body;
+    document.title = title;
+    window.history.pushState({ "html": html, "pageTitle": title }, "", urlPath);
+}
+
+window.onpopstate = function (e) {
+    if (e.state) {
+        document.getElementById("content").innerHTML = e.state.html;
+        document.title = e.state.pageTitle;
+    }
 };
-
-/*
-  Handles a click on the song played progress bar.
-*/
-document.getElementById('song-played-progress').addEventListener('click', function (e) {
-    var offset = this.getBoundingClientRect();
-    var x = e.pageX - offset.left;
-
-    Amplitude.setSongPlayedPercentage((parseFloat(x) / parseFloat(this.offsetWidth)) * 100);
-});
-
-document.querySelector('img[data-amplitude-song-info="cover_art_url"]').style.height = document.querySelector('img[data-amplitude-song-info="cover_art_url"]').offsetWidth + 'px';
-
-Amplitude.init({
-    "bindings": {
-        37: 'prev',
-        39: 'next',
-        32: 'play_pause'
-    },
-    "songs": [
-        {
-            "name": "Risin' High (feat Raashan Ahmad)",
-            "artist": "Ancient Astronauts",
-            "album": "We Are to Answer",
-            "url": "https://521dimensions.com/song/Ancient Astronauts - Risin' High (feat Raashan Ahmad).mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg"
-        },
-        {
-            "name": "The Gun",
-            "artist": "Lorn",
-            "album": "Ask The Dust",
-            "url": "https://521dimensions.com/song/08 The Gun.mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/ask-the-dust.jpg"
-        },
-        {
-            "name": "Anvil",
-            "artist": "Lorn",
-            "album": "Anvil",
-            "url": "https://521dimensions.com/song/LORN - ANVIL.mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/anvil.jpg"
-        },
-        {
-            "name": "I Came Running",
-            "artist": "Ancient Astronauts",
-            "album": "We Are to Answer",
-            "url": "https://521dimensions.com/song/ICameRunning-AncientAstronauts.mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg"
-        },
-        {
-            "name": "First Snow",
-            "artist": "Emancipator",
-            "album": "Soon It Will Be Cold Enough",
-            "url": "https://521dimensions.com/song/FirstSnow-Emancipator.mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/soon-it-will-be-cold-enough.jpg"
-        },
-        {
-            "name": "Terrain",
-            "artist": "pg.lost",
-            "album": "Key",
-            "url": "https://521dimensions.com/song/Terrain-pglost.mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/key.jpg"
-        },
-        {
-            "name": "Vorel",
-            "artist": "Russian Circles",
-            "album": "Guidance",
-            "url": "https://521dimensions.com/song/Vorel-RussianCircles.mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/guidance.jpg"
-        },
-        {
-            "name": "Intro / Sweet Glory",
-            "artist": "Jimkata",
-            "album": "Die Digital",
-            "url": "https://521dimensions.com/song/IntroSweetGlory-Jimkata.mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/die-digital.jpg"
-        },
-        {
-            "name": "Offcut #6",
-            "artist": "Little People",
-            "album": "We Are But Hunks of Wood Remixes",
-            "url": "https://521dimensions.com/song/Offcut6-LittlePeople.mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-but-hunks-of-wood.jpg"
-        },
-        {
-            "name": "Dusk To Dawn",
-            "artist": "Emancipator",
-            "album": "Dusk To Dawn",
-            "url": "https://521dimensions.com/song/DuskToDawn-Emancipator.mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/from-dusk-to-dawn.jpg"
-        },
-        {
-            "name": "Anthem",
-            "artist": "Emancipator",
-            "album": "Soon It Will Be Cold Enough",
-            "url": "https://521dimensions.com/song/Anthem-Emancipator.mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/soon-it-will-be-cold-enough.jpg"
-        }
-    ]
-});
