eef65da54d4f011bdcded6670867500b0a53cbcd
Paul Istratoaie <p4ul.gg@gmail.com>
2021-02-06 21:59:19 +0000

    feat(player): music library to load into player
    Functionality for loading the songs from the library into the player and auto generate the list in
    the player

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,3 +1,133 @@
+var globalPlaylist = {
+    "bindings": {
+        37: 'prev',
+        39: 'next',
+        32: 'play_pause'
+    },
+    "default_album_art": "https://pinco227.github.io/D-man/media/music/cover.jpg",
+    "songs": [
+        {
+            "name": "Si Ingerii Plang",
+            "artist": "Vineri 13",
+            "album": "Ingeri si Demoni",
+            "url": "https://pinco227.github.io/D-man/media/music/1999%20VINERI%2013%20-%20Ingeri%20si%20Demoni/Vineri%2013%20-%20Si%20ingerii%20plang.mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/1999%20VINERI%2013%20-%20Ingeri%20si%20Demoni/cover.jpg",
+            "duration": "04:30"
+        },
+        {
+            "name": "Joaca dupa reguli",
+            "artist": "Vineri 13",
+            "album": "Ne Doare La BassK",
+            "url": "https://pinco227.github.io/D-man/media/music/1998%20VINERI%2013%20-%20Ne%20doare%20la%20BassK/Vineri%2013%20%20-%20Joaca%20dupa%20reguli%20(1998).mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/1998%20VINERI%2013%20-%20Ne%20doare%20la%20BassK/cover.jpg",
+            "duration": "04:19"
+        },
+        {
+            "name": "Mana la cer feat. Ovi",
+            "artist": "Vineri 13",
+            "album": "Ingeri si Demoni",
+            "url": "https://pinco227.github.io/D-man/media/music/1999%20VINERI%2013%20-%20Ingeri%20si%20Demoni/Vineri%2013%20cu%20Ovi%20GENIUS%20-%20Mana%20la%20cer.mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/1998%20VINERI%2013%20-%20Ne%20doare%20la%20BassK/cover.jpg",
+            "duration": "03:39"
+        },
+        {
+            "name": "1, 2 si 3 de 4 ft PDV",
+            "artist": "DMAN, Dahey, Jonny, Mihai",
+            "album": "Singles",
+            "url": "https://pinco227.github.io/D-man/media/music/Singles/2000%20DMAN,%20DAHEY,%20JONNY,%20MIHAI%20ft%20PDV-%201%202%20%20si%203%20de%204%20.mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/Singles/cover.jpg",
+            "duration": "03:52"
+        },
+        {
+            "name": "diliMan",
+            "artist": "DMAN ft. SEBA",
+            "album": "DOMINO Project",
+            "url": "https://pinco227.github.io/D-man/media/music/2001%20DOMINO%20project/DMAN%20ft%20SEBA%20-%20diliMan.mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/2001%20DOMINO%20project/cover.jpg",
+            "duration": "03:33"
+        },
+        {
+            "name": "Amintirea ta",
+            "artist": "DMAN ft. Andrei",
+            "album": "Singles",
+            "url": "https://pinco227.github.io/D-man/media/music/Singles/2002%20DILIMANU%20ft%20ANDREI%20-%20Amintirea%20Ta.mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/Singles/cover.jpg",
+            "duration": "04:17"
+        },
+        {
+            "name": "Fak Ya Yo!",
+            "artist": "DMAN ft. Dahey",
+            "album": "Singles",
+            "url": "https://pinco227.github.io/D-man/media/music/Singles/2005%20DMAN%20ft%20DAHEY-%20Fak%20Ya%20Yo%20!.mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/Singles/cover.jpg",
+            "duration": "04:15"
+        },
+        {
+            "name": "Noi",
+            "artist": "DMAN",
+            "album": "Morphine",
+            "url": "https://pinco227.github.io/D-man/media/music/2008%20MORPHINE%20project/06.%20Morphine%20-%20Noi.mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/2008%20MORPHINE%20project/morphine.jpg",
+            "duration": "04:07"
+        },
+        {
+            "name": "Scrisoare de pe front",
+            "artist": "LORE ft. DMAN",
+            "album": "Singles",
+            "url": "https://pinco227.github.io/D-man/media/music/Singles/2014%20LORE%20ft%20DMAN%20-%20Scrisoare%20de%20pe%20front%20.mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/Singles/cover.jpg",
+            "duration": "04:00"
+        },
+        {
+            "name": "Inger si demon",
+            "artist": "Rosu si Negru ft. DMAN",
+            "album": "Singles",
+            "url": "https://pinco227.github.io/D-man/media/music/Singles/2015%20ROSUsi%20NEGRU%20cu%20DMAN%20-%20Inger%20si%20Demonn%20.mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/Singles/cover.jpg",
+            "duration": "04:37"
+        },
+        {
+            "name": "Las ca-i bine",
+            "artist": "DMAN",
+            "album": "Singles",
+            "url": "https://pinco227.github.io/D-man/media/music/Singles/2018%20DMAN%20-%20Las%20ca-i%20bine.mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/Singles/cover.jpg",
+            "duration": "02:56"
+        }
+    ]
+};
+
+function updatePlayList() {
+    var listDiv = document.getElementById('list');
+    listDiv.innerHTML = "";
+    var j = 1;
+    globalPlaylist.songs.forEach(function (song, i) {
+        listDiv.innerHTML += `
+        <div class="song amplitude-song-container amplitude-play-pause" data-amplitude-song-index="${i}">
+            <span class="song-number-now-playing">
+                <span class="number">${j}</span>
+                <img class="now-playing" src="https://521dimensions.com/img/open-source/amplitudejs/examples/flat-black/now-playing.svg" />
+            </span>
+
+            <div class="song-meta-container">
+                <span class="song-name" data-amplitude-song-info="name" data-amplitude-song-index="${i}"></span>
+                <span class="song-artist-album"><span data-amplitude-song-info="artist" data-amplitude-song-index="${i}"></span> - <span data-amplitude-song-info="album" data-amplitude-song-index="${i}"></span></span>
+            </div>
+            <span class="song-duration duration" data-amplitude-song-info="duration" data-amplitude-song-index="${i}"></span>
+        </div>
+        `;
+        j++;
+    });
+}
+
+function loadPlaylist(again) {
+    if (typeof again == "undefined") {
+        Amplitude.stop();
+    }
+    updatePlayList();
+    Amplitude.init(globalPlaylist);
+}
+
 window.onkeydown = function (e) {
     return !(e.keyCode == 32);
 };
@@ -12,92 +142,6 @@ document.getElementById('song-played-progress').addEventListener('click', functi
     Amplitude.setSongPlayedPercentage((parseFloat(x) / parseFloat(this.offsetWidth)) * 100);
 });
 
-document.querySelector('img[data-amplitude-song-info="cover_art_url"]').style.height = document.querySelector('img[data-amplitude-song-info="cover_art_url"]').offsetWidth + 'px';
+// document.querySelector('img[data-amplitude-song-info="cover_art_url"]').style.height = document.querySelector('img[data-amplitude-song-info="cover_art_url"]').offsetWidth + 'px';
 
-Amplitude.init({
-    "bindings": {
-        37: 'prev',
-        39: 'next',
-        32: 'play_pause'
-    },
-    "default_album_art": "https://pinco227.github.io/D-man/media/music/cover.jpg",
-    "songs": [
-        {
-            "name": "Si Ingerii Plang",
-            "artist": "Vineri 13",
-            "album": "Ingeri si Demoni",
-            "url": "https://pinco227.github.io/D-man/media/music/1999%20VINERI%2013%20-%20Ingeri%20si%20Demoni/Vineri%2013%20-%20Si%20ingerii%20plang.mp3",
-            "cover_art_url": "https://pinco227.github.io/D-man/media/music/1999%20VINERI%2013%20-%20Ingeri%20si%20Demoni/cover.jpg"
-        },
-        {
-            "name": "Joaca dupa reguli",
-            "artist": "Vineri 13",
-            "album": "Ne Doare La BassK",
-            "url": "https://pinco227.github.io/D-man/media/music/1998%20VINERI%2013%20-%20Ne%20doare%20la%20BassK/Vineri%2013%20%20-%20Joaca%20dupa%20reguli%20(1998).mp3",
-            "cover_art_url": "https://pinco227.github.io/D-man/media/music/1998%20VINERI%2013%20-%20Ne%20doare%20la%20BassK/cover.jpg"
-        },
-        {
-            "name": "Mana la cer feat. Ovi",
-            "artist": "Vineri 13",
-            "album": "Ingeri si Demoni",
-            "url": "https://pinco227.github.io/D-man/media/music/1999%20VINERI%2013%20-%20Ingeri%20si%20Demoni/Vineri%2013%20cu%20Ovi%20GENIUS%20-%20Mana%20la%20cer.mp3",
-            "cover_art_url": "https://pinco227.github.io/D-man/media/music/1998%20VINERI%2013%20-%20Ne%20doare%20la%20BassK/cover.jpg"
-        },
-        {
-            "name": "1, 2 si 3 de 4 ft PDV",
-            "artist": "DMAN, Dahey, Jonny, Mihai",
-            "album": "Singles",
-            "url": "https://pinco227.github.io/D-man/media/music/Singles/2000%20DMAN,%20DAHEY,%20JONNY,%20MIHAI%20ft%20PDV-%201%202%20%20si%203%20de%204%20.mp3",
-            "cover_art_url": "https://pinco227.github.io/D-man/media/music/Singles/cover.jpg"
-        },
-        {
-            "name": "diliMan",
-            "artist": "DMAN ft. SEBA",
-            "album": "DOMINO Project",
-            "url": "https://pinco227.github.io/D-man/media/music/2001%20DOMINO%20project/DMAN%20ft%20SEBA%20-%20diliMan.mp3",
-            "cover_art_url": "https://pinco227.github.io/D-man/media/music/2001%20DOMINO%20project/cover.jpg"
-        },
-        {
-            "name": "Amintirea ta",
-            "artist": "DMAN ft. Andrei",
-            "album": "Singles",
-            "url": "https://pinco227.github.io/D-man/media/music/Singles/2002%20DILIMANU%20ft%20ANDREI%20-%20Amintirea%20Ta.mp3",
-            "cover_art_url": "https://pinco227.github.io/D-man/media/music/Singles/cover.jpg"
-        },
-        {
-            "name": "Fak Ya Yo!",
-            "artist": "DMAN ft. Dahey",
-            "album": "Singles",
-            "url": "https://pinco227.github.io/D-man/media/music/Singles/2005%20DMAN%20ft%20DAHEY-%20Fak%20Ya%20Yo%20!.mp3",
-            "cover_art_url": "https://pinco227.github.io/D-man/media/music/Singles/cover.jpg"
-        },
-        {
-            "name": "Noi",
-            "artist": "DMAN",
-            "album": "Morphine",
-            "url": "https://pinco227.github.io/D-man/media/music/2008%20MORPHINE%20project/06.%20Morphine%20-%20Noi.mp3",
-            "cover_art_url": "https://pinco227.github.io/D-man/media/music/2008%20MORPHINE%20project/morphine.jpg"
-        },
-        {
-            "name": "Scrisoare de pe front",
-            "artist": "LORE ft. DMAN",
-            "album": "Singles",
-            "url": "https://pinco227.github.io/D-man/media/music/Singles/2014%20LORE%20ft%20DMAN%20-%20Scrisoare%20de%20pe%20front%20.mp3",
-            "cover_art_url": "https://pinco227.github.io/D-man/media/music/Singles/cover.jpg"
-        },
-        {
-            "name": "Inger si demon",
-            "artist": "Rosu si Negru ft. DMAN",
-            "album": "Singles",
-            "url": "https://pinco227.github.io/D-man/media/music/Singles/2015%20ROSUsi%20NEGRU%20cu%20DMAN%20-%20Inger%20si%20Demonn%20.mp3",
-            "cover_art_url": "https://pinco227.github.io/D-man/media/music/Singles/cover.jpg"
-        },
-        {
-            "name": "Las ca-i bine",
-            "artist": "DMAN",
-            "album": "Singles",
-            "url": "https://pinco227.github.io/D-man/media/music/Singles/2018%20DMAN%20-%20Las%20ca-i%20bine.mp3",
-            "cover_art_url": "https://pinco227.github.io/D-man/media/music/Singles/cover.jpg"
-        }
-    ]
-});
+loadPlaylist(1);
--- assets/js/script.js
+++ assets/js/script.js
@@ -52,11 +52,11 @@ function setPage(p) {
     document.title = titles[p] + ' | ' + mainTitle;
     contentDiv.innerHTML = routes[p];
     if (p === 'videos') {
-        writeVideosToDoc('yt-content');
+        writeVideosToDoc();
     } else if (p === 'gallery') {
-        writePhotosToDoc('gallery-content');
+        writePhotosToDoc();
     } else if (p === 'music') {
-        writeMusicToDoc('music-library');
+        writeMusicToDoc();
     }
 }
 
@@ -107,7 +107,15 @@ document.querySelectorAll('.nav-link').forEach(function (button) {
 });
 // ---------------------------------------------------------------------- SPA END  ----------
 
-// -------------------------------------------------------------- GENERAL API GET  ----------
+// ------------------------------------------------------------------- PAGES APIs  ----------
+// Declare API urls to call for GET
+// Youtube
+const youtubePlaylistApiUrl = 'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UU-pMGW4z9PyJ8R8J19tBzsg&key=AIzaSyASZKSW8QjCJPjuRvlaSzEA67IYSxENYe0';
+// Gallery
+const galleryApiUrl = 'https://pinco227.github.io/D-man/media/photos/list.json';
+// Music
+const musicApiUrl = 'https://pinco227.github.io/D-man/media/music/list.json';
+
 /** 
 * API GET function. Gets the data from the url and sends it as a parameter to the callback function.
 * @param {string} url - Api url to be called
@@ -124,7 +132,112 @@ function getData(url, cb) {
     xhr.open("GET", url);
     xhr.send();
 }
-// ------------------------------------------------------------ GENERAL API GET END ----------
+
+/**
+* Write the result of the Videos API call to the DOM
+* @param {string} contentDiv - The id of the element to be written to
+*/
+function writeVideosToDoc() {
+    getData(youtubePlaylistApiUrl, function (data) {
+        var writeTo = document.getElementById('yt-content');
+        data = data.items;
+        var list = document.createElement('ul');
+
+        data.forEach(function (item) {
+            var listItem = document.createElement('li');
+            listItem.innerHTML = `
+                <img src="${item.snippet.thumbnails.medium.url}" />
+            `;
+            list.appendChild(listItem);
+        });
+
+        writeTo.appendChild(list);
+    });
+}
+
+/** 
+* Write the result of the Photos API call to the DOM
+* @param {string} contentDiv - The id of the element to be written to
+*/
+function writePhotosToDoc() {
+    getData(galleryApiUrl, function (data) {
+        var writeTo = document.getElementById('gallery-content');
+        data = data.files;
+        var list = document.createElement('ul');
+
+        data.forEach(function (item) {
+            var listItem = document.createElement('li');
+            listItem.innerHTML = `
+                <img src="media/photos/${item.thumbnail}" alt="${item.title} - ${item.description}" />
+            `;
+            list.appendChild(listItem);
+        });
+
+        writeTo.appendChild(list);
+    });
+}
+/** 
+* Write the result of the Music API call to the DOM
+* @param {number} album - album index. If is set the function writes the songs of album. If is not set it writes the album list
+*/
+function writeMusicToDoc(album) {
+    getData(musicApiUrl, function (data) {
+        globalPlaylist.songs = [];
+        var writeTo = document.getElementById('music-library');
+        writeTo.innerHTML = '';
+        data = data.albums;
+
+        // Write the songs list of album
+        if (typeof album == "number") {
+            writeTo.innerHTML = `<button onClick="writeMusicToDoc()">Back</button>`;
+            var list = document.createElement('ul');
+            data[album].songs.forEach(function (item) {
+                var listItem = document.createElement('li');
+                listItem.innerHTML = `
+                    ${item.artist} - ${item.name}
+                `;
+                list.appendChild(listItem);
+
+                var song = {
+                    "name": item.name,
+                    "artist": item.artist,
+                    "album": data[album].name,
+                    "url": "/media/music/" + data[album].path + "/" + item.url,
+                    "cover_art_url": "/media/music/" + data[album].path + "/" + data[album].cover,
+                    "duration": item.duration
+                };
+                globalPlaylist.songs.push(song);
+            });
+            writeTo.appendChild(list);
+
+        } else { // writes the album list
+            var list = document.createElement('ul');
+            data.forEach(function (item, i) {
+                var listItem = document.createElement('li');
+                listItem.innerHTML = `
+                    <img src="media/music/${item.path}/${item.cover}" alt="${item.artist} - ${item.name}" width="100" onClick="writeMusicToDoc(${i});" />
+                `;
+                list.appendChild(listItem);
+
+                item.songs.forEach(function (sg) {
+                    var song = {
+                        "name": sg.name,
+                        "artist": sg.artist,
+                        "album": item.name,
+                        "url": "/media/music/" + item.path + "/" + sg.url,
+                        "cover_art_url": "/media/music/" + item.path + "/" + item.cover,
+                        "duration": sg.duration
+                    };
+                    globalPlaylist.songs.push(song);
+                });
+            });
+            writeTo.appendChild(list);
+        }
+        writeTo.innerHTML += `<button onClick="loadPlaylist()">Play</button>`;
+    });
+}
+
+// ---------------------------------------------------------------- PAGES APIs END ----------
 
 // Invoke the Main function
 main();
