4db1362b6f073a761ee94bf9d64114dbcf0bbc52
Paul Istratoaie <p4ul.gg@gmail.com>
2021-02-06 15:17:47 +0000

    refactor: update initial playlist with local songs

--- assets/js/player.js
+++ assets/js/player.js
@@ -20,84 +20,84 @@ Amplitude.init({
         39: 'next',
         32: 'play_pause'
     },
-    "default_album_art": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg",
+    "default_album_art": "https://pinco227.github.io/D-man/media/music/cover.jpg",
     "songs": [
         {
-            "name": "Risin' High (feat Raashan Ahmad)",
-            "artist": "Ancient Astronauts",
-            "album": "We Are to Answer",
-            "url": "https://521dimensions.com/song/Ancient Astronauts - Risin' High (feat Raashan Ahmad).mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg"
+            "name": "Si Ingerii Plang",
+            "artist": "Vineri 13",
+            "album": "Ingeri si Demoni",
+            "url": "https://pinco227.github.io/D-man/media/music/1999%20VINERI%2013%20-%20Ingeri%20si%20Demoni/Vineri%2013%20-%20Si%20ingerii%20plang.mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/1999%20VINERI%2013%20-%20Ingeri%20si%20Demoni/cover.jpg"
         },
         {
-            "name": "The Gun",
-            "artist": "Lorn",
-            "album": "Ask The Dust",
-            "url": "https://521dimensions.com/song/08 The Gun.mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/ask-the-dust.jpg"
+            "name": "Joaca dupa reguli",
+            "artist": "Vineri 13",
+            "album": "Ne Doare La BassK",
+            "url": "https://pinco227.github.io/D-man/media/music/1998%20VINERI%2013%20-%20Ne%20doare%20la%20BassK/Vineri%2013%20%20-%20Joaca%20dupa%20reguli%20(1998).mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/1998%20VINERI%2013%20-%20Ne%20doare%20la%20BassK/cover.jpg"
         },
         {
-            "name": "Anvil",
-            "artist": "Lorn",
-            "album": "Anvil",
-            "url": "https://521dimensions.com/song/LORN - ANVIL.mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/anvil.jpg"
+            "name": "Mana la cer feat. Ovi",
+            "artist": "Vineri 13",
+            "album": "Ingeri si Demoni",
+            "url": "https://pinco227.github.io/D-man/media/music/1999%20VINERI%2013%20-%20Ingeri%20si%20Demoni/Vineri%2013%20cu%20Ovi%20GENIUS%20-%20Mana%20la%20cer.mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/1998%20VINERI%2013%20-%20Ne%20doare%20la%20BassK/cover.jpg"
         },
         {
-            "name": "I Came Running",
-            "artist": "Ancient Astronauts",
-            "album": "We Are to Answer",
-            "url": "https://521dimensions.com/song/ICameRunning-AncientAstronauts.mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-to-answer.jpg"
+            "name": "1, 2 si 3 de 4 ft PDV",
+            "artist": "DMAN, Dahey, Jonny, Mihai",
+            "album": "Singles",
+            "url": "https://pinco227.github.io/D-man/media/music/Singles/2000%20DMAN,%20DAHEY,%20JONNY,%20MIHAI%20ft%20PDV-%201%202%20%20si%203%20de%204%20.mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/Singles/cover.jpg"
         },
         {
-            "name": "First Snow",
-            "artist": "Emancipator",
-            "album": "Soon It Will Be Cold Enough",
-            "url": "https://521dimensions.com/song/FirstSnow-Emancipator.mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/soon-it-will-be-cold-enough.jpg"
+            "name": "diliMan",
+            "artist": "DMAN ft. SEBA",
+            "album": "DOMINO Project",
+            "url": "https://pinco227.github.io/D-man/media/music/2001%20DOMINO%20project/DMAN%20ft%20SEBA%20-%20diliMan.mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/2001%20DOMINO%20project/cover.jpg"
         },
         {
-            "name": "Terrain",
-            "artist": "pg.lost",
-            "album": "Key",
-            "url": "https://521dimensions.com/song/Terrain-pglost.mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/key.jpg"
+            "name": "Amintirea ta",
+            "artist": "DMAN ft. Andrei",
+            "album": "Singles",
+            "url": "https://pinco227.github.io/D-man/media/music/Singles/2002%20DILIMANU%20ft%20ANDREI%20-%20Amintirea%20Ta.mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/Singles/cover.jpg"
         },
         {
-            "name": "Vorel",
-            "artist": "Russian Circles",
-            "album": "Guidance",
-            "url": "https://521dimensions.com/song/Vorel-RussianCircles.mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/guidance.jpg"
+            "name": "Fak Ya Yo!",
+            "artist": "DMAN ft. Dahey",
+            "album": "Singles",
+            "url": "https://pinco227.github.io/D-man/media/music/Singles/2005%20DMAN%20ft%20DAHEY-%20Fak%20Ya%20Yo%20!.mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/Singles/cover.jpg"
         },
         {
-            "name": "Intro / Sweet Glory",
-            "artist": "Jimkata",
-            "album": "Die Digital",
-            "url": "https://521dimensions.com/song/IntroSweetGlory-Jimkata.mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/die-digital.jpg"
+            "name": "Noi",
+            "artist": "DMAN",
+            "album": "Morphine",
+            "url": "https://pinco227.github.io/D-man/media/music/2008%20MORPHINE%20project/06.%20Morphine%20-%20Noi.mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/2008%20MORPHINE%20project/morphine.jpg"
         },
         {
-            "name": "Offcut #6",
-            "artist": "Little People",
-            "album": "We Are But Hunks of Wood Remixes",
-            "url": "https://521dimensions.com/song/Offcut6-LittlePeople.mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/we-are-but-hunks-of-wood.jpg"
+            "name": "Scrisoare de pe front",
+            "artist": "LORE ft. DMAN",
+            "album": "Singles",
+            "url": "https://pinco227.github.io/D-man/media/music/Singles/2014%20LORE%20ft%20DMAN%20-%20Scrisoare%20de%20pe%20front%20.mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/Singles/cover.jpg"
         },
         {
-            "name": "Dusk To Dawn",
-            "artist": "Emancipator",
-            "album": "Dusk To Dawn",
-            "url": "https://521dimensions.com/song/DuskToDawn-Emancipator.mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/from-dusk-to-dawn.jpg"
+            "name": "Inger si demon",
+            "artist": "Rosu si Negru ft. DMAN",
+            "album": "Singles",
+            "url": "https://pinco227.github.io/D-man/media/music/Singles/2015%20ROSUsi%20NEGRU%20cu%20DMAN%20-%20Inger%20si%20Demonn%20.mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/Singles/cover.jpg"
         },
         {
-            "name": "Anthem",
-            "artist": "Emancipator",
-            "album": "Soon It Will Be Cold Enough",
-            "url": "https://521dimensions.com/song/Anthem-Emancipator.mp3",
-            "cover_art_url": "https://521dimensions.com/img/open-source/amplitudejs/album-art/soon-it-will-be-cold-enough.jpg"
+            "name": "Las ca-i bine",
+            "artist": "DMAN",
+            "album": "Singles",
+            "url": "https://pinco227.github.io/D-man/media/music/Singles/2018%20DMAN%20-%20Las%20ca-i%20bine.mp3",
+            "cover_art_url": "https://pinco227.github.io/D-man/media/music/Singles/cover.jpg"
         }
     ]
 });
